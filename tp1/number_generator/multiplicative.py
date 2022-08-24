6de859c5cff154c914ac9fd05ba70b4816c486cd
ToledoRodrigo <toledoarodrigo@gmail.com>
2022-08-23 23:35:31 -0300

    Last minute fixes

--- tp1/number_generator/multiplicative.py
+++ tp1/number_generator/multiplicative.py
@@ -2,14 +2,10 @@ from generation.number_generator.base import BaseNumberGenerator
 from generation.utils import truncate
 
 class Multiplicative(BaseNumberGenerator):
-    def __init__(self, a, m, x0):
-        self.x = x0
+    def __init__(self, a, m):
         self.a = a
         self.m = m
         self.c = 0
-
-    def generate_number(self):
-        return self.x
 
     def generate_number(self, seed, *args, **kwargs):
         return (self.c + self.a * seed) % self.m
